a158e555b0dc31965aa1eb670bebe47b061432c7
MaksimTikhonenko <danchukmaks1736@gmail.com>
2024-06-04 15:36:25 +0300

    new directori and task loops.py completed

---
+++ Cmpleted/loops.py
@@ -0,0 +1,11 @@
+
+#  Task
+#  The provided code stub reads and integer,"n", from STDIN. For all non-negative integers "i<n" print "i*i"
+
+n = int(3)
+i = 0
+
+while i < n:
+    print(i * i)
+    i = i + 1
+
