54b57de8e1ff075299d9ab68ea1c81d63a78ccf8
MaksimTikhonenko <danchukmaks1736@gmail.com>
2024-09-23 14:54:28 +0300

    work on Word_order

--- Word_order/test_order.py
+++ Word_order/test_order.py
@@ -1,2 +1,11 @@
 import pytest
+from The_word_order import word_order
+
+
+def test_word_order():
+    numbers_to_print = ['2', '1', '1']
+    words = ["bcdef", "abcdefg", "bcde", "bcdef"]
+    assert word_order(words) == (4, '2 1 1')
+    # assert word_order("") is None
+    # assert word_order(None) is None
 
